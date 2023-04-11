ed1cc4445583c35960ad11a35bd6ae1174113b19
Manuel Gonzalez <manuelg9704@gmail.com>
2023-04-11 15:28:32 -0500

    variables de entorno para la db

--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,10 @@
 import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mtm',
+  host: process.env.HOSTNAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 export default connection;
