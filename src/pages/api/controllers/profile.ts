223d2381c2c6fd9710805cfc63e3eebfa3ffff99
Manuel Gonzalez <manuelg9704@gmail.com>
2023-04-14 14:48:57 -0500

    arreglando tipos de Base de datos con rowDatapocket

--- src/pages/api/controllers/profile.ts
+++ src/pages/api/controllers/profile.ts
@@ -3,13 +3,17 @@ import { verify } from 'jsonwebtoken';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function profileHandler(req: NextApiRequest, res: NextApiResponse) {
+  interface userPayload {
+    documento: string;
+    username: string;
+  }
   const { myTokenName } = req.cookies;
   if (!myTokenName) {
     return res.status(401).json({ error: 'NO token' });
   }
 
   try {
-    const user = verify(myTokenName, 'secret');
+    const user = verify(myTokenName, 'secret') as userPayload;
     return res.json({ documento: user.documento, username: user.username });
   } catch (error) {
     return res.status(401).json({ error: 'Invalid token' });
--- src/pages/api/models/deleteUser.ts
+++ src/pages/api/models/deleteUser.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+import { RowDataPacket } from 'mysql2';
 import db from '../../../db';
 
 interface UserCount {
@@ -7,7 +8,10 @@ interface UserCount {
 export async function deleteUser(documento: string): Promise<void> {
   // Consulta SQL para contar usuarios con el documento especificado
   const countUserQuery = 'SELECT COUNT(*) as count FROM usuario WHERE documento = ?';
-  const data: UserCount[] = await db.query(countUserQuery, [documento]);
+  const result = await db.query(countUserQuery, [documento]);
+  const data: UserCount[] = (result[0] as RowDataPacket[]).map((row) => ({
+    count: row.count as number,
+  }));
   // Utilizando el primer elemento del array con el índice cero en lugar de usar "first" de lodash
   const primeroArr = data[0];
   if (primeroArr && primeroArr.count === 0) {
