b59c47f3b4c3bde4dc6d4adfb76383d4d9af15fb
Manuel Gonzalez <manuelg9704@gmail.com>
2023-04-14 14:58:56 -0500

    arreglando tipos de base de datos SQL

--- src/pages/api/models/newUser.ts
+++ src/pages/api/models/newUser.ts
@@ -5,13 +5,20 @@
 import { first } from 'lodash';
 import { QUERY_COUNT_CHECK_BY_DOCUMENT } from '@/querysMySQLproyectomtm';
 import { Usuario } from '@/tiposproyectomtm';
+import { RowDataPacket } from 'mysql2';
 import db from '../../../db';
 
 export async function insertUser(user: Usuario): Promise<any> {
-  const data = await db.query(QUERY_COUNT_CHECK_BY_DOCUMENT, [user.documento]);
+  interface UserCount {
+    count: number;
+  }
+  const result = await db.query(QUERY_COUNT_CHECK_BY_DOCUMENT, [user.documento]);
   // const data = await db.query('SELECT COUNT(*) as count FROM usuario WHERE documento = ?', [user.documento]);
+  const data: UserCount[] = (result[0] as RowDataPacket[]).map((row) => ({
+    count: row.count as number,
+  }));
   const primeroArr = first(data);
-  if (first(primeroArr).count > 0) {
+  if (primeroArr && primeroArr.count > 0) {
     throw new Error('El usuario ya existe');
   }
   await db.query(
